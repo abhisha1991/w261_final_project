dac621e457a106a0c4cd511e8664f64835ed913d
Abhi Sharma <abhisha@microsoft.com>
2021-07-13 01:31:39 -0700

    added up to q7

--- HW5/hw5_workbook.py
+++ HW5/hw5_workbook.py
@@ -765,7 +765,7 @@ print(f'... initialization continued: {time.time() - start} seconds')
 
 # MAGIC %md ### Q8 Student Answers:
 # MAGIC 
-# MAGIC > __a)__ There is a `1/|G|` chance of landing at any particular page, where `|G|` is the number of nodes in the graph. Probability $\alpha is the likelihood that the random surfer executes a random jump (also called the teleportation factor), and 1-$\alpha is the probability that the random surfer follows a hyperlink (also called the damping factor).
+# MAGIC > __a)__ There is a `1/|G|` chance of landing at any particular page, where `|G|` is the number of nodes in the graph. Probability $\alpha$ is the likelihood that the random surfer executes a random jump (also called the teleportation factor), and 1-$\alpha$ is the probability that the random surfer follows a hyperlink (also called the damping factor).
 # MAGIC 
 # MAGIC 
 # MAGIC > __b)__ `m` is the dangling node PageRank mass that is "lost" due to being "black-holed" within the dangling node. This is the mass we need to distribute back into the graph across all the nodes to preserve our page rank calculations, and avoid getting "stuck" at the dangline node. We do this redistribution by dividing the mass by |G| (length of the graph) and adding it to the original page rank probability of the node (in the damping term of PageRank caclulation). Note that non-dangling nodes will have `m=0`, so they will only contribute their PageRank mass. 
